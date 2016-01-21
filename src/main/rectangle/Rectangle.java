a3f57f4b58856cab9ce9b2892fb12f014d82de63
priti <priti.biyani6@gmail.com>
2016-01-21 23:21:53 +0530

    No longer using square

--- src/main/rectangle/Rectangle.java
+++ src/main/rectangle/Rectangle.java
@@ -4,7 +4,7 @@ public class Rectangle implements Shape {
     private final double breadth;
     private final double length;
 
-    public Rectangle(double length, double breadth) throws IllegalStateException {
+    private Rectangle(double length, double breadth) throws IllegalStateException {
         if (length <= 0 || breadth <= 0) {
             throw new IllegalStateException("Length or breadth can not be negative or 0");
         }
@@ -25,5 +25,9 @@ public class Rectangle implements Shape {
 
     public static Shape getSquare(double side) {
         return new Rectangle(side, side);
+    }
+
+    public static Rectangle getRectangle(double length, double breadth) {
+            return new Rectangle(length, breadth);
     }
 }
--- src/test/rectangle/RectangleTest.java
+++ src/test/rectangle/RectangleTest.java
@@ -1,6 +1,7 @@
 package rectangle;
 
 import org.junit.Test;
+import org.w3c.dom.css.Rect;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -13,13 +14,13 @@ public class RectangleTest {
 
     @Test
     public void shouldCalculatePerimeter(){
-        Rectangle rectangle = new Rectangle(3.4, 4.1);
+        Rectangle rectangle = Rectangle.getRectangle(3.4, 4.1);
         assertEquals(15.0, rectangle.calculatePerimeter());
     }
 
     @Test
     public void shouldCalculateAreaGivenValidSide() {
-        Rectangle rectangle  = new  Rectangle(5.0, 2.0);
+        Rectangle rectangle  = Rectangle.getRectangle(5.0, 2.0);
         assertEquals(10.0, rectangle.calculateArea());
     }
 
